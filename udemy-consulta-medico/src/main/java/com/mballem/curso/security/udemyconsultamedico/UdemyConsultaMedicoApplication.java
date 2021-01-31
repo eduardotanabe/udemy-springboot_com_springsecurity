539db40f1fcdd46967687fab3a5e0e22a9fd8f31
Eduardo Tanabe <e_tanabe@yahoo.com.br>
2021-01-31 17:22:59 -0400

    Projeto inicial

---
+++ udemy-consulta-medico/src/main/java/com/mballem/curso/security/udemyconsultamedico/UdemyConsultaMedicoApplication.java
@@ -0,0 +1,13 @@
+package com.mballem.curso.security.udemyconsultamedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyConsultaMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyConsultaMedicoApplication.class, args);
+	}
+
+}
---
+++ udemy-consulta-medico/src/test/java/com/mballem/curso/security/udemyconsultamedico/UdemyConsultaMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mballem.curso.security.udemyconsultamedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyConsultaMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
